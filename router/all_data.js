bfef4efa9b3b2dc1096cad16ebc531f88751d37b
YOT4BIT3 <127468485+AbinKrishna57@users.noreply.github.com>
2025-06-05 16:07:01 +0530

    Update all_data.js

--- router/all_data.js
+++ router/all_data.js
@@ -5,8 +5,8 @@ const mysql=require("mysql2")
 const db=mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Server###Beast69#',
-  database: 'Exp'
+  password: '<password>',
+  database: '<database>'
 })
 
 db.connect((err)=>{
